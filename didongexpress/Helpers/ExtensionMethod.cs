28f0e1da77852df3f10d846460154b68de8249ff
quang <oclockvn@gmail.com>
2016-12-19 23:05:49 +0700

    upload first time

---
+++ didongexpress/Helpers/ExtensionMethod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace didongexpress
+{
+    public static class ExtensionMethod
+    {
+        public static string Prefix(this string original, string prefix)
+        {
+            // check null or empty, etc...
+
+            return "[" + prefix + "] " + original;
+        }
+    }
+}
---
+++ didongexpress/gulpfile.js
@@ -0,0 +1,22 @@
+﻿var gulp = require('gulp'),
+    scss = require('gulp-ruby-sass'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    notify = require('gulp-notify');
+
+// styles task
+var cssTask = gulp.task('css', function () {
+    return scss('./Content/styles/styles.scss')
+        .pipe(postcss([autoprefixer({ browsers: ['last 2 versions'] })]))
+        .pipe(gulp.dest('./Content/styles/'))
+        .pipe(notify({ message: 'css task completed' }));
+});
+
+// js task
+
+// images task
+
+// watch
+gulp.task('watch', function () {
+    gulp.watch('./Content/styles/styles.scss', ['css']); // watch file `./Content/styles/styles.scss` and run task `css`
+});
