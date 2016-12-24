1b49136be850d9dce1394be2bed0319a0506847f
quang <oclockvn@gmail.com>
2016-12-24 18:33:49 +0700

    added shopping cart

---
+++ didongexpress/Scripts/app/app.js
@@ -0,0 +1,15 @@
+﻿(function (w) {
+    'use strict';
+
+    var app = {
+        isDebug: true,
+        log: function () {
+            if (this.isDebug) {
+                var arr = Array.prototype.slice.apply(arguments);
+                console.log(arr);
+            }
+        }
+    };
+
+    window.app = app;
+})(window);
--- didongexpress/Scripts/app/common.js
+++ didongexpress/Scripts/app/common.js
@@ -1,11 +1,58 @@
-﻿//(function ($, document, window, undefined) {
+﻿
+
+//(function ($, document, window, undefined) {
 
     
 
 //})(jQuery, document, window, undefined);
 
+function scrollTop() {
+
+    /*
+     * use jquery animate is ok,
+     * 
+     * but if you care about performance, dont use it
+     * 
+     * use...velocity instead
+     */
+
+    /*
+    $('html, body').animate({
+        scrollTop: '0'
+    }, 500);
+    */
+
+    // velocity version
+    $('html').velocity('scroll', { offset: "0", mobileHA: false });
+
+    // show/hide button after scroll
+    // we can use callback
+    // but there is a better solution
+}
+
 $(document).ready(function () {
+
+    var $document = $(document),
+        $scroll_top_button = $('#button-scroll-top');
+
     $('.bxslider').bxSlider({
         controls: false
     });
+
+    $document.on('click', '#button-scroll-top', scrollTop);
+
+    $('#button-scroll-top').waypoint(function (direction) {
+        // show if down
+        if (direction === 'down') {
+            $scroll_top_button.addClass('show-up');
+            app.log('show button', 1, 2, true);
+            app.log('hello %s', 'Quang', { name: 'kratos' });
+        } else { // hide if up
+            $scroll_top_button.removeClass('show-up');
+            app.log('hide button');
+        }
+    }, {
+        offset: '200px' // start action if offset top = 200px
+    });
+    
 });
