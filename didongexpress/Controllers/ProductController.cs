4020b7bd948beee10310c7dbba088d7449f6ec57
quang <oclockvn@gmail.com>
2017-01-05 00:03:09 +0700

    add product detail

---
+++ didongexpress/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace didongexpress.Controllers
+{
+    public class ProductController : Controller
+    {        
+        public ActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
